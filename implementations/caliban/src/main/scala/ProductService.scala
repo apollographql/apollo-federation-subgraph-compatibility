2bb3f7c8a8440d8b224745d28bd6d111b3b63c3c
Paul Daniels <paulpdaniels@gmail.com>
2021-08-22 19:26:57 +0800

    Add caliban implementation

---
+++ implementations/caliban/src/main/scala/ProductService.scala
@@ -0,0 +1,47 @@
+import zio.{ Ref, UIO }
+
+trait ProductService {
+  def getProductById(id: String): UIO[Option[Product]]
+  def getProductBySkuAndPackage(sku: String, pack: String): UIO[Option[Product]]
+  def getProductBySkuAndVariationId(sku: String, variationId: String): UIO[Option[Product]]
+}
+
+object ProductService {
+  val inMemory =
+    Ref
+      .make(
+        List(
+          Product(
+            id = ID("apollo-federation"),
+            sku = Some("federation"),
+            `package` = Some("@apollo/federation"),
+            variation = Some(ProductVariation(ID("OSS"))),
+            dimensions = Some(ProductDimension(Some("1"), Some(1.0f))),
+            createdBy = Some(User(ID("support@apollographql.com"), Some(42)))
+          ),
+          Product(
+            id = ID("apollo-studio"),
+            sku = Some("studio"),
+            `package` = Some(""),
+            variation = Some(ProductVariation(ID("platform"))),
+            dimensions = Some(ProductDimension(Some("1"), Some(1.0f))),
+            createdBy = Some(User(ID("support@apollographql.com"), Some(42)))
+          )
+        )
+      )
+      .map { products =>
+        new ProductService {
+          override def getProductById(id: String): UIO[Option[Product]] =
+            products.get.map(_.find(_.id.id == id))
+
+          override def getProductBySkuAndPackage(sku: String, pack: String): UIO[Option[Product]] =
+            products.get.map(_.find(p => p.sku.contains(sku) && p.`package`.contains(pack)))
+
+          override def getProductBySkuAndVariationId(sku: String, variationId: String): UIO[Option[Product]] =
+            products.get.map(
+              _.find(p => p.sku.contains(sku) && p.variation.contains(ProductVariation(ID(variationId))))
+            )
+        }
+      }
+      .toLayer
+}
