e8e6e1b9a227f29220e22700416e8be3827e9ea2
Michael Watson <watson@apollographql.com>
2021-08-30 12:02:18 -0700

    Add expect to provides test
    - Also support piping debug flags into VSCode debug console

--- src/tests/provides.test.ts
+++ src/tests/provides.test.ts
@@ -1,6 +1,6 @@
-import { productsRequest } from "../utils/client";
+import { productsRequest } from '../utils/client';
 
-test("@provides", async () => {
+test('@provides', async () => {
   const resp = await productsRequest({
     query: `#graphql
       query ($id: ID!) {
@@ -8,17 +8,21 @@ test("@provides", async () => {
           createdBy { email totalProductsCreated }
         }
       }`,
-    variables: { id: "apollo-federation" },
+    variables: { id: 'apollo-federation' },
   });
+
+  const totalProductsCreated: number =
+    resp.data.product.createdBy.totalProductsCreated;
 
   expect(resp).toMatchObject({
     data: {
       product: {
         createdBy: {
-          email: "support@apollographql.com",
-          totalProductsCreated: 1337,
+          email: 'support@apollographql.com',
+          totalProductsCreated: expect.any(totalProductsCreated),
         },
       },
     },
   });
+  expect(totalProductsCreated).not.toEqual(4);
 });
