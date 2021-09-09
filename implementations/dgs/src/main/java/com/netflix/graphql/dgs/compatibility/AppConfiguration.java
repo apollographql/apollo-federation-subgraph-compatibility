c2aa63bcfc10d31df835b295d9f81eee7a95496f
Adam Setch <adam.setch@outlook.com>
2021-09-09 11:58:15 -0400

    Implement dgs ftv1

---
+++ implementations/dgs/src/main/java/com/netflix/graphql/dgs/compatibility/AppConfiguration.java
@@ -0,0 +1,14 @@
+package com.netflix.graphql.dgs.compatibility;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.apollographql.federation.graphqljava.tracing.FederatedTracingInstrumentation;
+
+
+@Configuration
+public class AppConfiguration {
+    @Bean
+    public graphql.execution.instrumentation.Instrumentation federatedTracingInstrumentation() {
+        return new FederatedTracingInstrumentation();
+    }
+}
