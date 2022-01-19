62dfbd323641f3a9c3ca1c7f523ee7bd9ba12309
Michael Staib <michael@chillicream.com>
2022-01-19 11:02:32 +0100

    Added Hot Chocolate GraphQL Server

---
+++ implementations/hotchocolate/Models/Query.cs
@@ -0,0 +1,10 @@
+﻿using HotChocolate.ApolloFederation;
+
+namespace Products;
+
+[ForeignServiceTypeExtension]
+public class Query
+{
+    public Product? GetProduct([ID] string id, Data repository)
+        => repository.Products.FirstOrDefault(t => t.Id.Equals(id));
+}
