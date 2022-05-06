d6aa0c13dd5a0a34e5d1604d878b8b61af1eee54
Martin Bonnin <martin@mbonnin.net>
2022-05-06 15:32:50 +0200

    federation-jvm:2.0.0-alpha.6-SNAPSHOT

--- src/tests/tag.test.ts
+++ src/tests/tag.test.ts
@@ -8,7 +8,7 @@ test("@tag", async () => {
   });
 
   const { sdl } = response.data._service;
-  expect(stripIgnoredCharacters(sdl)).toContain('@tag(name:"internal")');
+  expect(stripIgnoredCharacters(sdl)).toContain('@apollo_tag(name:"internal")');
 
   expect(compareSchemas(response.data?._service?.sdl)).toBe(true);
 });
