d40aad16409406668d34ebe337b6bd369b021bf8
Damien Pontifex <damien.pontifex@gmail.com>
2022-06-09 07:19:15 +0800

    Update async-graphql library versions

--- implementations/async-graphql/src/main.rs
+++ implementations/async-graphql/src/main.rs
@@ -1,18 +1,18 @@
 use actix_web::{post, web, App, HttpServer};
-use async_graphql_actix_web::{Request, Response};
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
 use std::net::Ipv4Addr;
 
 mod graphql;
 use crate::graphql::{create_schema, ProductSchema};
 
 #[post("/")]
-async fn index(schema: web::Data<ProductSchema>, req: Request) -> Response {
+async fn index(schema: web::Data<ProductSchema>, req: GraphQLRequest) -> GraphQLResponse {
     schema.execute(req.into_inner()).await.into()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().data(create_schema()).service(index))
+    HttpServer::new(|| App::new().app_data(create_schema()).service(index))
         .bind((Ipv4Addr::UNSPECIFIED, 4001))?
         .run()
         .await
