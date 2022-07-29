49f1d5477604907561e371fdab4dd039d0ccd2c3
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2022-07-29 17:12:10 -0500

    @inaccessible and @tag directive cannot be namespaced
    Add explicit tests that verify we don't namespace those two directives.

--- implementations/federation-jvm/src/main/java/com/apollographql/federation/compatibility/model/ProductDimension.java
+++ implementations/federation-jvm/src/main/java/com/apollographql/federation/compatibility/model/ProductDimension.java
@@ -3,10 +3,12 @@ package com.apollographql.federation.compatibility.model;
 public class ProductDimension {
     private final String size;
     private final float weight;
+    private final String unit;
 
-    public ProductDimension(String size, float weight) {
+    public ProductDimension(String size, float weight, String unit) {
         this.size = size;
         this.weight = weight;
+        this.unit = unit;
     }
 
     public String getSize() {
--- src/tests/tag.test.ts
+++ src/tests/tag.test.ts
@@ -8,7 +8,9 @@ test("@tag", async () => {
   });
 
   const { sdl } = response.data._service;
-  expect(stripIgnoredCharacters(sdl)).toContain('@tag(name:"internal")');
+  const normalizedSDL = stripIgnoredCharacters(sdl);
+  expect(normalizedSDL).not.toContain('@federation__tag');
+  expect(normalizedSDL).toContain('@tag(name:"internal")');
 
   expect(compareSchemas(response.data?._service?.sdl)).toBe(true);
 });
