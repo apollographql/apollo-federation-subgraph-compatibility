a20e01addc08a22cd83bb3116a8e57434a2c6e42
enisdenjo <badurinadenis@gmail.com>
2022-10-08 18:15:06 +0200

    custom resolvers instead of new query fields

--- implementations/graphql-mesh/src/schema.ts
+++ implementations/graphql-mesh/src/schema.ts
@@ -1,86 +1,12 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { readFileSync } from "node:fs";
-import { Product, ProductResearch, Resolvers, User } from "./resolvers-types";
-
-const productResearch: ProductResearch[] = [
-  {
-    study: {
-      caseNumber: "1234",
-      description: "Federation Study",
-    },
-  },
-  {
-    study: {
-      caseNumber: "1235",
-      description: "Studio Study",
-    },
-  },
-];
-
-const products: Product[] = [
-  {
-    id: "apollo-federation",
-    sku: "federation",
-    package: "@apollo/federation",
-    variation: { id: "OSS", __typename: "ProductVariation" },
-    research: [], // will resolve
-  },
-  {
-    id: "apollo-studio",
-    sku: "studio",
-    package: "",
-    variation: { id: "platform", __typename: "ProductVariation" },
-    research: [], // will resolve
-  },
-];
-
-const deprecatedProduct = {
-  sku: "apollo-federation-v1",
-  package: "@apollo/federation-v1",
-  reason: "Migrate to Federation V2",
-};
-
-const user: User = {
-  email: "support@apollographql.com",
-  name: "Jane Smith",
-  totalProductsCreated: 1337,
-  yearsOfEmployment: 10,
-};
+import { Resolvers } from "./resolvers-types";
+import { products, deprecatedProduct, productResearch, user } from "./data";
 
 const resolvers: Resolvers = {
   Query: {
     product(_, args) {
       return products.find((p) => p.id == args.id) || null;
-    },
-    resolveProduct(_, args) {
-      if (args.id) {
-        return products.find((product) => product.id === args.id) || null;
-      }
-      if (args.sku && args.package) {
-        return (
-          products.find(
-            (product) =>
-              product.sku === args.sku && product.package === args.package
-          ) || null
-        );
-      }
-      if (args.sku && args.variationId) {
-        return (
-          products.find(
-            (product) =>
-              product.sku === args.sku &&
-              product.variation?.id === args.variationId
-          ) || null
-        );
-      }
-      return null;
-    },
-    resolveProductResearch(_, args) {
-      return (
-        productResearch.find(
-          (p) => p.study.caseNumber === args.studyCaseNumber
-        ) || null
-      );
     },
     deprecatedProduct(_, args) {
       if (
