2c145e240c038a9e8dac218907dfffcfbecc054f
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2023-01-12 17:44:14 -0600

    fix filenames

--- test_all.sh
+++ test_all.sh
@@ -9,11 +9,11 @@ do
     echo "${impl}"
 
     npm run compatibility:test -- docker --debug --compose implementations/${impl}/docker-compose.yaml --schema implementations/_template_library_/products.graphql --format json
-    jq -s '.[0] + {tests: .[1]}' implementations/${impl}/metadata.json results.json > results_${impl}.json
+    jq -s '.[0] + {tests: .[1]}' implementations/${impl}/metadata.json results.json > results-${impl}.json
   fi
 done
 
 # merge results into single file
-jq -s . results_*.json > final_results.json
+jq -s . results*.json > final_results.json
 
 npm run compatibility:report -- final_results.json
