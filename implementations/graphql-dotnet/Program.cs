b5eb02f3f932fc4a9e33088cf27f35f6b11d78e8
Dariusz Kuc <9501705+dariuszkuc@users.noreply.github.com>
2023-01-19 01:05:37 -0600

    subgraph: update graphql-dotnet example to use latest schema (#329)
    Resolves: https://github.com/apollographql/apollo-federation-subgraph-compatibility/issues/186

--- implementations/graphql-dotnet/Program.cs
+++ implementations/graphql-dotnet/Program.cs
@@ -1,7 +1,6 @@
-using GraphQL.Server;
+using GraphQL;
 using GraphQL.Types;
 using GraphQL.Utilities.Federation;
-using Microsoft.Extensions.DependencyInjection;
 using ApolloFederation.SubgraphCompatibility.Products;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +9,6 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(builder =>
         builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod())
 );
-
 
 // Add GraphQL builder.Services and configure options
 builder.Services
@@ -27,7 +25,7 @@ builder.Services
             schemaBuilder.Types.For(nameof(Product))
                 .ResolveReferenceAsync<Product?>(ctx =>
                 {
-                    var productId = ctx.Arguments["id"]?.ToString();
+                    var productId = ctx.Arguments.GetValueOrDefault("id")?.ToString();
                     if (!string.IsNullOrEmpty(productId))
                     {
                         return Task.FromResult<Product?>(Data.Products.FirstOrDefault(p => p.Id == productId));
@@ -35,29 +33,79 @@ builder.Services
 
                     return Task.FromResult<Product?>(null);
                 });
+            schemaBuilder.Types.For(nameof(DeprecatedProduct))
+                .ResolveReferenceAsync<DeprecatedProduct?>(ctx =>
+                {
+                    var sku = ctx.Arguments["sku"]?.ToString();
+                    var pkg = ctx.Arguments["package"]?.ToString();
+                    if (!string.IsNullOrEmpty(sku) && !string.IsNullOrEmpty(pkg))
+                    {
+                        return Task.FromResult(Data.DeprecatedProducts.FirstOrDefault(p => p.Sku == sku && p.Package == pkg));
+                    }
+
+                    return Task.FromResult<DeprecatedProduct?>(null);
+                });
+            schemaBuilder.Types.For(nameof(User))
+                .ResolveReferenceAsync<User?>(ctx =>
+                {
+                    var email = ctx.Arguments["email"]?.ToString();
+                    if (!string.IsNullOrEmpty(email))
+                    {
+                        var user = Data.Users.FirstOrDefault(u => u.Email == email);
+                        if (user != null)
+                        {
+                            string? totalProductsCreated = ctx.Arguments.GetValueOrDefault("totalProductsCreated")?.ToString();
+                            if (totalProductsCreated != null)
+                            {
+                                user = user with { TotalProductsCreated = int.Parse(totalProductsCreated) };
+                            }
+
+                            string? yearsOfEmployment = ctx.Arguments.GetValueOrDefault("yearsOfEmployment")?.ToString();
+                            if (yearsOfEmployment != null)
+                            {
+                                user.LengthOfEmployment = int.Parse(yearsOfEmployment);
+                            }
+                        }
+                        return Task.FromResult(user);
+                    }
+
+                    return Task.FromResult<User?>(null);
+                });
+            schemaBuilder.Types.For(nameof(ProductResearch))
+                .ResolveReferenceAsync<ProductResearch?>(ctx =>
+                {
+                    var study = ctx.Arguments.GetValueOrDefault("study");
+                    if (study is Dictionary<string, object>)
+                    {
+                        var caseNumber = ((Dictionary<string, object>)study).GetValueOrDefault("caseNumber")?.ToString();
+                        return Task.FromResult(Data.ProductResearches.FirstOrDefault(r => r.Study.CaseNumber == caseNumber));
+                    }
+
+                    return Task.FromResult<ProductResearch?>(null);
+                });
         });
     })
-    .AddGraphQL((options, provider) =>
+    .AddGraphQL(graphqlBuilder =>
     {
-        var env = provider.GetRequiredService<IWebHostEnvironment>();
-        options.EnableMetrics = env.IsDevelopment();
-        var logger = provider.GetRequiredService<ILogger<ISchema>>();
-        options.UnhandledExceptionDelegate = ctx => logger.LogError("{Error} occured", ctx.OriginalException.Message);
-    })
-    .AddSystemTextJson(deserializerSettings => { }, serializerSettings => { })
-    .AddErrorInfoProvider((opt, provider) =>
-    {
-        var env = provider.GetRequiredService<IWebHostEnvironment>();
-        opt.ExposeExceptionStackTrace = env.IsDevelopment();
-    })
-    .AddDataLoader();
+
+        graphqlBuilder.UseApolloTracing();
+        // graphqlBuilder.ConfigureExecutionOptions(action =>
+        // {
+        //     action.UnhandledExceptionDelegate =
+
+        // });
+        graphqlBuilder.AddSystemTextJson();
+        graphqlBuilder.AddErrorInfoProvider(action =>
+        {
+            action.ExposeExceptionDetails = true;
+        });
+    });
 
 var app = builder.Build();
 
 app.UseCors();
-
 app.UseRouting();
-
-app.UseGraphQL<ISchema>("/");
+app.UseGraphQL("/");
 
 app.Run();
+// app.Run("http://localhost:4001");
