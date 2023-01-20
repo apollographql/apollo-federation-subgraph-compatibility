e04b24d50d793bbb3ddf53e15863d703664db4f7
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2023-01-20 14:36:24 -0600

    update hotchocolate example with latest schema changes
    See https://github.com/apollographql/apollo-federation-subgraph-compatibility/issues/166 for details.
    
    Resolves https://github.com/apollographql/apollo-federation-subgraph-compatibility/issues/189

---
+++ implementations/hotchocolate/Types/ProductResearch.cs
@@ -0,0 +1,27 @@
+using HotChocolate.ApolloFederation;
+
+namespace Products;
+
+[Key("study { caseNumber }")]
+public class ProductResearch
+{
+
+    public ProductResearch(CaseStudy study, string? outcome)
+    {
+        Study = study;
+        Outcome = outcome;
+    }
+
+    public CaseStudy Study { get; }
+    public string? Outcome { get; }
+
+    [ReferenceResolver]
+    public static ProductResearch? GetProductReasearchByCaseNumber(
+        [Map("study.caseNumber")] string caseNumber,
+        Data repository)
+    {
+        Console.WriteLine("case number = {0}", caseNumber);
+        return repository.ProductResearches.FirstOrDefault(
+            r => r.Study.CaseNumber.Equals(caseNumber));
+    }
+}
