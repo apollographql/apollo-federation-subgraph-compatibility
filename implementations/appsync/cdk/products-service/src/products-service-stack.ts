7dfac0c535a6b411b2140db57d19e8a246bd1713
Ahmed Elsenousi <aelsenou@amazon.com>
2023-10-09 15:36:15 +0100

    upgrade to cdk v2 (#527)
    Just need to upgrade the appsync implementation to use CDK v2

--- implementations/appsync/cdk/products-service/src/products-service-stack.ts
+++ implementations/appsync/cdk/products-service/src/products-service-stack.ts
@@ -1,17 +1,36 @@
-import * as cdk from '@aws-cdk/core';
-import * as appsync from '@aws-cdk/aws-appsync';
-import * as lambda from '@aws-cdk/aws-lambda-nodejs';
-import {Tracing} from '@aws-cdk/aws-lambda';
-
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as appsync from 'aws-cdk-lib/aws-appsync';
+import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Tracing } from 'aws-cdk-lib/aws-lambda';
+import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
+
 export class ProductsServiceStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const apolloSchema = readFileSync(join(__dirname, 'products-service.graphql'), { encoding: 'utf8' });
+    const appsyncSchema = apolloSchema.replace('scalar _Any', `
+        input _Any {
+            __typename: String!
+            id: String
+            sku: String
+            package: String
+            variation: ProductVariationInput
+            email: String
+            study: CaseStudyInput
+            totalProductsCreated: Int
+            yearsOfEmployment: Int
+        }
+      `).replace(/^extend schema.*{query: Query}/sm, '');
+      const appsyncSchemaPath = join(__dirname, 'appsync-compatible-schema.graphql');
+      writeFileSync(appsyncSchemaPath, appsyncSchema);
 
     const api = new appsync.GraphqlApi(this, 'Api', {
       name: 'products-service',
-      schema: appsync.Schema.fromAsset(join(__dirname, `products-service.graphql`)),
+      schema: appsync.SchemaFile.fromAsset(appsyncSchemaPath),
       authorizationConfig: {
         defaultAuthorization: {
           authorizationType: appsync.AuthorizationType.API_KEY,
@@ -23,28 +42,28 @@ export class ProductsServiceStack extends cdk.Stack {
     const lambdaResolver = new lambda.NodejsFunction(this, 'lambdaResolver', {
       entry: join(__dirname, `products-service-resolver.ts`),
       environment: {
-        SCHEMA: api.schema.definition.replace('__typename: String!', ''),
+        SCHEMA: apolloSchema
       },
       tracing: Tracing.ACTIVE,
     });
 
     const lambdaDS = api.addLambdaDataSource('LambdaDS', lambdaResolver);
 
-    lambdaDS.createResolver({
+    lambdaDS.createResolver('QueryService', {
       typeName: 'Query',
       fieldName: '_service',
     });
 
-    lambdaDS.createResolver({
+    lambdaDS.createResolver('QueryEntities', {
       typeName: 'Query',
       fieldName: '_entities',
     });
 
-    lambdaDS.createResolver({
+    lambdaDS.createResolver('QueryProduct', {
       typeName: 'Query',
       fieldName: 'product'});
 
-    lambdaDS.createResolver({
+    lambdaDS.createResolver('QueryProductCreatedBy', {
       typeName: 'Product',
       fieldName: 'createdBy'
     });
