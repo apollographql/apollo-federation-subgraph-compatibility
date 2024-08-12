2db1226bb97e48a29b8d6863a8f6aff39ed52e99
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2024-08-12 14:37:56 -0500

    chore: update to use latest router

--- packages/compatibility/src/composeSupergraph.ts
+++ packages/compatibility/src/composeSupergraph.ts
@@ -8,9 +8,9 @@ import { readFile, writeFile } from 'fs/promises';
 import { createWriteStream } from 'fs';
 
 const COMPOSITION_VERSION =
-  process.env['APOLLO_ROVER_DEV_COMPOSITION_VERSION'] ?? '2.7.1';
+  process.env['APOLLO_ROVER_DEV_COMPOSITION_VERSION'] ?? '2.7.8';
 const ROUTER_VERSION =
-  process.env['APOLLO_ROVER_DEV_ROUTER_VERSION'] ?? '1.39.0';
+  process.env['APOLLO_ROVER_DEV_ROUTER_VERSION'] ?? '1.52.0';
 const roverDebug = debug('rover');
 
 /**
