994e614af5e18b5933b3bd8d57b65d5a60c70e24
Artur Gordashnikov <gao.artur@gmail.com>
2025-09-10 15:55:54 +0300

    cleanup

--- implementations/graphql-dotnet/Program.cs
+++ implementations/graphql-dotnet/Program.cs
@@ -1,20 +1,13 @@
 using ApolloFederation.SubgraphCompatibility.Products.Types;
 using GraphQL;
-using GraphQL.Federation.Types;
 using GraphQL.Types;
 using GraphQLParser.AST;
-using GraphQLParser.Visitors;
-using ServiceLifetime = GraphQL.DI.ServiceLifetime;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-    options.AddDefaultPolicy(policyBuilder =>
-        policyBuilder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod())
-);
-
 builder.Services
     .AddSingleton<Query>()
+    .AddSingleton<Data>()
     .AddGraphQL(b => b
         .AddSystemTextJson()
         .AddAutoSchema<Query>()
@@ -29,23 +22,10 @@ builder.Services
             schema.LinkSchema("https://myspecs.dev/myCustomDirective/v1.0", link => link.Imports.Add("@custom", "@custom"));
             schema.ApplyDirective("composeDirective", "name", "@custom");
         })
-        .ConfigureExecutionOptions(options => options.ThrowOnUnhandledException = true)
-        .Services.Register<Data>(ServiceLifetime.Singleton));
+        .ConfigureExecutionOptions(options => options.ThrowOnUnhandledException = true));
 
 var app = builder.Build();
 
-app.MapGet("/print", context =>
-{
-    var schema = context.RequestServices.GetRequiredService<ISchema>().Print(new FederationPrintOptions
-    {
-        ArgumentsPrintMode = SDLPrinterArgumentsMode.ForceNewLine,
-        IncludeImportedDefinitions = false,
-    });
-    context.Response.WriteAsync(schema);
-    return Task.CompletedTask;
-});
-
-app.UseCors();
 app.UseRouting();
 app.UseGraphQL("/");
 
