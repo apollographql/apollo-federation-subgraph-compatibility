0e7a9ca4364c3a4e6eae2fcb8fc60bffc1910dcf
Artur Gordashnikov <gao.artur@gmail.com>
2025-09-21 20:05:06 +0300

    rename the local schema to products

--- implementations/graphql-dotnet/Program.cs
+++ implementations/graphql-dotnet/Program.cs
@@ -29,7 +29,7 @@ builder.Services
 var app = builder.Build();
 
 // Sample: print the schema to a file
-File.WriteAllText("schema.graphql", app.Services.GetRequiredService<ISchema>().Print());
+File.WriteAllText("products.graphql", app.Services.GetRequiredService<ISchema>().Print());
 
 app.UseRouting();
 app.UseGraphQL("/");
